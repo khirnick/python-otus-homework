58d518b25e5b88b50c93574fced4c03d1a6114a1
Nikita Khirny <khirnick@yandex.ru>
2024-12-14 16:18:31 +0300

    Add RBAC

--- ml_model_serving/src/ml_model_serving/handlers/login.py
+++ ml_model_serving/src/ml_model_serving/handlers/login.py
@@ -1,67 +1,114 @@
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
-from fastapi.exceptions import HTTPException
-from fastapi.params import Depends
-from fastapi.security.oauth2 import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
 from pydantic import BaseModel
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+SECRET_KEY = "b58c233fd38ca23503e2a084fa45f2bdf8e108ee5050423e52e7b17fe37b2985"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 fake_users_db = {
-    'johndoe': {
-        'username': 'johndoe',
-        'full_name': 'John Doe',
-        'email': 'johndoe@example.com',
-        'hashed_password': 'fakehashedsecret',
-        'disabled': False,
+    "johndoe": {
+        "username": "johndoe",
+        "role": "admin",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
     },
-    'alice': {
-        'username': 'alice',
-        'full_name': 'Alice Wonderson',
-        'email': 'alice@example.com',
-        'hashed_password': 'fakehashedsecret2',
-        'disabled': True,
-    },
-}
+    "johndoe": {
+        "username": "tomtom",
+        "role": "user",
+        "full_name": "Tom Tom",
+        "email": "tomtom@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+} 
 
 
-def fake_hash_password(password: str) -> str:
-    return 'fakehashed' + password
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
 
 
 class User(BaseModel):
-
     username: str
+    role: str
     email: str | None = None
     full_name: str | None = None
     disabled: bool | None = None
 
 
 class UserInDB(User):
-
     hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
+    
 
-
-def fake_decode_token(token):
-    user = get_user(fake_users_db, token)
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
     return user
 
 
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
 
@@ -73,12 +120,32 @@ async def get_current_active_user(
     return current_user
 
 
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail='Incorrect username or password')
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail='Incorrect username or password')
-    return {'access_token': user.username, 'token_type': 'bearer'}
+class RoleChecker:
+
+    def __init__(self, allowed_roles):  
+        self.allowed_roles = allowed_roles  
+  
+    def __call__(self, user: Annotated[User, Depends(get_current_active_user)]):  
+        if user.role in self.allowed_roles:  
+            return True  
+        raise HTTPException(  
+            status_code=status.HTTP_401_UNAUTHORIZED,   
+            detail="You don't have enough permissions",
+        )
+
+
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
--- ml_model_serving/src/ml_model_serving/handlers/models.py
+++ ml_model_serving/src/ml_model_serving/handlers/models.py
@@ -3,7 +3,7 @@ from fastapi.exceptions import HTTPException
 from fastapi.params import Depends, Path
 from pydantic.dataclasses import dataclass
 
-from ml_model_serving.handlers.login import get_current_active_user, User
+from ml_model_serving.handlers.login import RoleChecker, get_current_active_user, User
 from ml_model_serving.models import models
 
 
@@ -16,7 +16,7 @@ class Features:
 def model_inference(
     model_name: Annotated[str, Path(title='ML Model Name', min_length=3, max_length=20)],
     features: Features,
-    current_user: Annotated[User, Depends(get_current_active_user)]
+    rbac: Annotated[bool, Depends(RoleChecker(allowed_roles=["admin"]))],
 ) -> dict[str, float]:
     if model_name not in models:
         raise HTTPException(status_code=404, detail=f'Model {model_name} not found')
